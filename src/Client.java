9e4fc67125eb450e229c03a443d34676ea2b4324
theicarus1632 <47543491+theicarus1632@users.noreply.github.com>
2019-03-24 18:15:27 -0400

    comments

--- src/Client.java
+++ src/Client.java
@@ -1,7 +1,9 @@
+//this class is a representation of the client in the database 
 public class Client extends People {
 
     private int referralNo;
 
+    //this is the client object constructor 
     public Client(int id,
                   String name,
                   String address,
@@ -12,6 +14,7 @@ public class Client extends People {
         this.referralNo = referralNo;
     }
 
+    //this method returns a client based on the data given 
     public Client(String[] data){
         this(Integer.parseInt(data[0]), data[1], data[2], data[3], data[4], Integer.parseInt(data[6]));
     }
@@ -20,6 +23,7 @@ public class Client extends People {
         return referralNo;
     }
 
+    //TODO: 
     /*    public static void main(String[] args) {
         Client c = new Client(1, "s", "s", "s", "s");
         String s = c.getAddress();

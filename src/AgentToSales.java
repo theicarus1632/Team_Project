d801391d496eddbb5455ef812a03278420f4eec6
theicarus1632 <47543491+theicarus1632@users.noreply.github.com>
2019-03-24 18:10:36 -0400

    comments

--- src/AgentToSales.java
+++ src/AgentToSales.java
@@ -1,12 +1,15 @@
+//this class associates an agent with a specific sale
 public class AgentToSales {
     private int agentID;
     private int salesID;
 
+    //this is the agent-sales constructor 
     public AgentToSales(int agentID, int salesID){
         this.agentID = agentID;
         this.salesID = salesID;
     }
 
+    // returns an AgentToSales based on the data given 
     public AgentToSales(String[] data){
         this(Integer.parseInt(data[0]), Integer.parseInt(data[1]));
     }
